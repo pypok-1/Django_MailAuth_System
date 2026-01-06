9d4f945b2f382f13cb7685ed0cbb3381105c5d0f
Eva <evacherkashyna9@gmail.com>
2026-01-06 19:59:35 +0200

    Update views.py

--- project/user/views.py
+++ project/user/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
@@ -17,7 +18,33 @@ def register_user(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            return redirect()
+            return redirect('login')
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'GET':
+        form = AuthenticationForm(request)
+        return render(request, 'users/login.html', {'form': form})
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('')
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+
+
+
 
 
 
