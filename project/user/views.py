9200b4a43bf44190c72b07d4e48c1da21cf35a12
Eva <evacherkashyna9@gmail.com>
2026-01-06 16:51:08 +0200

    Update views.py

--- project/user/views.py
+++ project/user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import UserForm
+
+
+def register_user(request):
+    if request.method == 'GET':
+        form = UserForm()
+        return render(request, 'users/register.html', {'form': form})
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect()
+
+
+
