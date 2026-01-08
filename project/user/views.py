0005924c796da2c3a9770db52bbaf363e64f7f56
Eva <evacherkashyna9@gmail.com>
2026-01-08 22:39:04 +0200

    Update views.py

--- project/user/views.py
+++ project/user/views.py
@@ -1,10 +1,8 @@
+from email.message import EmailMessage
 from django.conf import settings
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .forms import UserForm
 
 
@@ -12,24 +10,7 @@ def register_user(request):
     if request.method == 'GET':
         form = UserForm()
         return render(request, 'users/register.html', {'form': form})
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            admin_email(user)
-            welcome_email(user)
-            return redirect('home')
 
-    return render(request, 'users/register.html', {'form': form})
-
-
-def home_view(request):
-    return render(request, 'users/home.html')
-
-
-def admin_email(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
@@ -37,19 +18,29 @@ def admin_email(request: HttpRequest) -> HttpResponse:
             user.set_password(form.cleaned_data['password'])
             user.save()
             send_mail(
-                subject = "Нова реєстрація на сайті",
-                message = f"Новий користувач: {user.username} | Email: {user.email}",
-                from_email= 'test@test.com',
-                recipient_list = ['admin@example.com'],
+                subject="Нова реєстрація на сайті",
+                message=f"Новий користувач: {user.username} | Email: {user.email}",
+                from_email='test@test.com',
+                recipient_list=['admin@example.com'],
             )
+            html_content = render_to_string(
+                'emails/user_welcome.html',
+                {'username': user.username}
+            )
+            email = EmailMessage(
+                subject="Ласкаво просимо!",
+                body=html_content,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=[user.email],
+                cc=[settings.ADMIN_EMAIL],
+                bcc=[settings.ADMIN_EMAIL],
+            )
+            email.content_subtype = "html"
+            email.send()
             return redirect('home')
 
-
-def welcome_email(user):
-
+    return render(request, 'users/register.html', {'form': form})
 
 
-
-
-
-
+def home_view(request):
+    return render(request, 'users/home.html')
